503040b09e6391e7af8930bc369e8248a14d2783
Hackons la corruption <pascal.robichaud.do101@gmail.com>
2015-07-09 21:27:05 -0400

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -5,14 +5,14 @@ r = requests.get('https://fairemtl.ca/fr/affichage-dynamique-vers-stationnement-
 
 if (r.status_code == requests.codes.ok):
 
-  print(r.encoding)
+  #print(r.encoding)
   r.encoding = 'ISO-8859-1'
-  print(r.encoding)
+  #print(r.encoding)
   
   #print(r.url)
   #print(r.headers)
   #print(r.content)
-  #print(r.text)
+  print(r.text)
   
   
   
