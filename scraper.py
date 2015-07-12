e461b86698e8db8d3054ee03623bafc6a789ea02
Hackons la corruption <pascal.robichaud.do101@gmail.com>
2015-07-12 13:19:22 -0400

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -18,7 +18,6 @@ from bs4 import BeautifulSoup
 # Page à scraper
 #source_url = 'https://fairemtl.ca/fr/affichage-dynamique-vers-stationnement-disponible'
 source_url = 'https://fairemtl.ca/fr/24h-linnovation-volet-ville-intelligente'
-print('-'*60)
 print("Page: %s" % source_url)
 html = urllib.urlopen(source_url)
 
@@ -32,7 +31,7 @@ onglet_commentaires = onglet_commentaires.replace("Commentaires (","")
 onglet_commentaires = onglet_commentaires.replace(")","")
 
 # Afficher le nombre de commentaires
-print("Nombre de commentaires: %s" % onglet_commentaires)	   
+print("Nombre de commentaires: %s" % onglet_commentaires.decode('utf-8'))	   
 
 #2. Nombre d'abonnés
 #nombre_abonnes = soup.find_all('div',{'class':'pill js-subscribe_section_content'}, encode="utf-8")
